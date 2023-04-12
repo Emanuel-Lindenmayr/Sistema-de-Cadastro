fb3ed03048df4dee4b3df6ec1d04664fd3abcba8
Emanuel-Lindenmayr <emanuellindenmayr@hotmail.com>
2023-04-12 20:13:35 -0300

    commit-inicial

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'Controller/PessoaController.php';
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// "espinha dorsal" do MVC
+switch($url) 
+{
+    case '/':
+        echo "pagina inicial";
+    break;
+    
+    case '/pessoa':
+        PessoaController::index();
+    break;
+
+    case '/pessoa/form':
+        PessoaController::form();
+    break;
+
+    default:
+        echo "error 404";
+    break;
+}
